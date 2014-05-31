cf91de701ab686a970e0057d3e868cde507efcfe
Alex Suraci <suraci.alex@gmail.com>
2014-05-31 14:10:27 -0700

    dedup author/committer metadata

--- assets/common.bash
+++ assets/common.bash
@@ -16,12 +16,28 @@ function load_pubkey() {
 }
 
 function git_metadata() {
-  jq -n "[
-    {name: \"commit\", value: $(git rev-parse HEAD | jq -R .)},
-    {name: \"author\", value: $(git log -1 --format=format:%an | jq -s -R .)},
-    {name: \"author_date\", value: $(git log -1 --format=format:%ai | jq -R .)},
-    {name: \"committer\", value: $(git log -1 --format=format:%cn | jq -s -R .)},
-    {name: \"committer_date\", value: $(git log -1 --format=format:%ci | jq -R .)},
-    {name: \"message\", value: $(git log -1 --format=format:%B | jq -s -R .)}
-  ]"
+  local commit=$(git rev-parse HEAD | jq -R .)
+  local author=$(git log -1 --format=format:%an | jq -s -R .)
+  local author_date=$(git log -1 --format=format:%ai | jq -R .)
+  local committer=$(git log -1 --format=format:%cn | jq -s -R .)
+  local committer_date=$(git log -1 --format=format:%ci | jq -R .)
+  local message=$(git log -1 --format=format:%B | jq -s -R .)
+
+  if [ "$author" = "$committer" ] && [ "$author_date" = "$committer_date" ]; then
+    jq -n "[
+      {name: \"commit\", value: ${commit}},
+      {name: \"author\", value: ${author}},
+      {name: \"author_date\", value: ${author_date}, type: \"time\"},
+      {name: \"message\", value: ${message}, type: \"message\"}
+    ]"
+  else
+    jq -n "[
+      {name: \"commit\", value: ${commit}},
+      {name: \"author\", value: ${author}},
+      {name: \"author_date\", value: ${author_date}, type: \"time\"},
+      {name: \"committer\", value: ${committer}},
+      {name: \"committer_date\", value: ${committer_date}, type: \"time\"},
+      {name: \"message\", value: ${message}, type: \"message\"}
+    ]"
+  fi
 }
