e2489dd6e65f2c06daf03d3ae36de606eab579b6
Alex Suraci <suraci.alex@gmail.com>
2015-03-21 15:36:29 -0700

    expand test suite to cover checking paths
    [#90575178]

--- test/check.bash
+++ test/check.bash
@@ -15,7 +15,7 @@ function it_can_check_from_a_ref() {
   local ref2=$(make_commit $repo)
   local ref3=$(make_commit $repo)
 
-  check_uri_ref $repo $ref1 | jq -e "
+  check_uri_from $repo $ref1 | jq -e "
     . == [
       {ref: $(echo $ref2 | jq -R .)},
       {ref: $(echo $ref3 | jq -R .)}
@@ -28,11 +28,118 @@ function it_can_check_from_a_bogus_sha() {
   local ref1=$(make_commit $repo)
   local ref2=$(make_commit $repo)
 
-  check_uri_ref $repo "bogus-ref" | jq -e "
+  check_uri_from $repo "bogus-ref" | jq -e "
     . == [{ref: $(echo $ref2 | jq -R .)}]
   "
 }
 
-with_tmpdir it_can_check_from_head
-with_tmpdir it_can_check_from_a_ref
-with_tmpdir it_can_check_from_a_bogus_sha
+function it_skips_ignored_paths() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to file-a $repo)
+  local ref2=$(make_commit_to file-b $repo)
+  local ref3=$(make_commit_to file-c $repo)
+
+  check_uri_ignoring $repo "file-c" | jq -e "
+    . == [{ref: $(echo $ref2 | jq -R .)}]
+  "
+
+  check_uri_from_ignoring $repo $ref1 "file-c" | jq -e "
+    . == [{ref: $(echo $ref2 | jq -R .)}]
+  "
+
+  local ref4=$(make_commit_to file-b $repo)
+
+  check_uri_ignoring $repo "file-c" | jq -e "
+    . == [{ref: $(echo $ref4 | jq -R .)}]
+  "
+
+  check_uri_from_ignoring $repo $ref1 "file-c" | jq -e "
+    . == [
+      {ref: $(echo $ref2 | jq -R .)},
+      {ref: $(echo $ref4 | jq -R .)}
+    ]
+  "
+}
+
+function it_checks_given_paths() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to file-a $repo)
+  local ref2=$(make_commit_to file-b $repo)
+  local ref3=$(make_commit_to file-c $repo)
+
+  check_uri_paths $repo "file-c" | jq -e "
+    . == [{ref: $(echo $ref3 | jq -R .)}]
+  "
+
+  check_uri_from_paths $repo $ref1 "file-c" | jq -e "
+    . == [{ref: $(echo $ref3 | jq -R .)}]
+  "
+
+  local ref4=$(make_commit_to file-b $repo)
+
+  check_uri_paths $repo "file-c" | jq -e "
+    . == [{ref: $(echo $ref3 | jq -R .)}]
+  "
+
+  local ref5=$(make_commit_to file-c $repo)
+
+  check_uri_from_paths $repo $ref1 "file-c" | jq -e "
+    . == [
+      {ref: $(echo $ref3 | jq -R .)},
+      {ref: $(echo $ref5 | jq -R .)}
+    ]
+  "
+}
+
+function it_checks_given_and_ignored_paths() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to file-a $repo)
+  local ref2=$(make_commit_to file-b $repo)
+  local ref3=$(make_commit_to some-file $repo)
+
+  check_uri_paths_ignoring $repo 'file-*' 'file-b' | jq -e "
+    . == [{ref: $(echo $ref1 | jq -R .)}]
+  "
+
+  check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' | jq -e "
+    . == []
+  "
+
+  local ref4=$(make_commit_to file-b $repo)
+
+  check_uri_paths_ignoring $repo 'file-*' 'file-b' | jq -e "
+    . == [{ref: $(echo $ref1 | jq -R .)}]
+  "
+
+  local ref5=$(make_commit_to file-a $repo)
+
+  check_uri_paths_ignoring $repo 'file-*' 'file-b' | jq -e "
+    . == [{ref: $(echo $ref5 | jq -R .)}]
+  "
+
+  local ref6=$(make_commit_to file-c $repo)
+
+  local ref7=$(make_commit_to some-file $repo)
+
+  check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' | jq -e "
+    . == [
+      {ref: $(echo $ref5 | jq -R .)},
+      {ref: $(echo $ref6 | jq -R .)}
+    ]
+  "
+
+  check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' 'file-c' | jq -e "
+    . == [
+      {ref: $(echo $ref5 | jq -R .)}
+    ]
+  "
+}
+
+run it_can_check_from_head
+run it_can_check_from_a_ref
+run it_can_check_from_a_bogus_sha
+run it_skips_ignored_paths
+run it_checks_given_paths
+run it_checks_given_and_ignored_paths
+
+echo $'\e[32mall tests passed!\e[0m'
--- test/helpers.bash
+++ test/helpers.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e -x
+set -e
 
 set -o pipefail
 
@@ -17,30 +17,13 @@ function cleanup() {
   fi
 }
 
-function with_tmpdir() {
+function run() {
   export TMPDIR=$(mktemp -d /tmp/git-tests.XXXXXX)
   trap "cleanup $TMPDIR" EXIT
 
-  eval "$@"
-}
-
-function check_uri() {
-  jq -n "{
-    source: {
-      uri: $(echo $1 | jq -R .)
-    }
-  }" | ${assets_dir}/check | tee /dev/stderr
-}
-
-function check_uri_ref() {
-  jq -n "{
-    source: {
-      uri: $(echo $1 | jq -R .)
-    },
-    version: {
-      ref: $(echo $2 | jq -R .)
-    }
-  }" | ${assets_dir}/check | tee /dev/stderr
+  echo $'running \e[33m'"$@"$'\e[0m...'
+  eval "$@" 2>&1 | sed -e 's/^/  /g'
+  echo ""
 }
 
 function init_repo() {
@@ -52,9 +35,126 @@ function init_repo() {
   )
 }
 
+function make_commit_to() {
+  echo x >> $2/$1
+  git -C $2 add $1
+  git -C $2 commit -q -m "commit $(wc -l $2/$1)"
+  git -C $2 rev-parse HEAD
+}
+
 function make_commit() {
-  echo x >> $1/some-file
-  git -C $1 add some-file
-  git -C $1 commit -q -m "commit $(wc -l $1/some-file)"
-  git -C $1 rev-parse HEAD
+  make_commit_to some-file $1
+}
+
+function check_uri() {
+  jq -n "{
+    source: {
+      uri: $(echo $1 | jq -R .)
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_ignoring() {
+  local uri=$1
+
+  shift
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      ignore_paths: $(echo "$@" | jq -R '. | split(" ")')
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_paths() {
+  local uri=$1
+
+  shift
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      paths: $(echo "$@" | jq -R '. | split(" ")')
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_paths_ignoring() {
+  local uri=$1
+  local paths=$2
+
+  shift 2
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      paths: [$(echo $paths | jq -R .)],
+      ignore_paths: $(echo "$@" | jq -R '. | split(" ")')
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_from() {
+  jq -n "{
+    source: {
+      uri: $(echo $1 | jq -R .)
+    },
+    version: {
+      ref: $(echo $2 | jq -R .)
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_from_ignoring() {
+  local uri=$1
+  local ref=$2
+
+  shift 2
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      ignore_paths: $(echo "$@" | jq -R '. | split(" ")')
+    },
+    version: {
+      ref: $(echo $ref | jq -R .)
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_from_paths() {
+  local uri=$1
+  local ref=$2
+
+  shift 2
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      paths: $(echo "$@" | jq -R '. | split(" ")')
+    },
+    version: {
+      ref: $(echo $ref | jq -R .)
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
+}
+
+function check_uri_from_paths_ignoring() {
+  local uri=$1
+  local ref=$2
+  local paths=$3
+
+  shift 3
+
+  jq -n "{
+    source: {
+      uri: $(echo $uri | jq -R .),
+      paths: [$(echo $paths | jq -R .)],
+      ignore_paths: $(echo "$@" | jq -R '. | split(" ")')
+    },
+    version: {
+      ref: $(echo $ref | jq -R .)
+    }
+  }" | ${assets_dir}/check | tee /dev/stderr
 }
