090f5a36baa0f2bc45cf45a5f0ffbbc814694626
Francis <fralalonde@expedia.com>
2016-04-15 22:42:57 -0400

    using amd64 version of git-lfs instead of 386

--- scripts/install_git_lfs.sh
+++ scripts/install_git_lfs.sh
@@ -7,7 +7,7 @@ _main() {
   tmpdir="$(mktemp -d git_lfs_install.XXXXXX)"
 
   cd "$tmpdir"
-  curl -Lo git.tar.gz https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-386-1.1.2.tar.gz
+  curl -Lo git.tar.gz https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz
   gunzip git.tar.gz
   tar xf git.tar
   mv git-lfs-1.1.2/git-lfs /usr/bin
