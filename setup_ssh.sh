f84cb17339b41c5409cb3e3f6b72c51ad4c77300
Piers Powlesland <piers.powlesland@yoti.com>
2017-08-10 01:26:40 +0100

    checkpoint commit, kinda working
    not sure about if i can really say I am doing the hop

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -11,6 +11,7 @@ make_user(){
 	perl -p -i -e "s|($1.*?)\!(.*)|\1*\2|" /etc/shadow
 
 	mkdir /home/$1/.ssh
+	chown $1 /home/$1/.ssh
 	cat $2 > /home/$1/.ssh/authorized_keys
 }
 
@@ -18,14 +19,30 @@ make_user(){
 ssh-keygen -A
 # set up authorized keys for each user to be under that users .ssh dir
 perl -p -i -e 's|AuthorizedKeysFile.*|AuthorizedKeysFile %h/.ssh/authorized_keys|' /etc/ssh/sshd_config
-/usr/sbin/sshd
+/usr/sbin/sshd -E /var/log/sshd -o 'LogLevel VERBOSE'
+
+mkdir -p /root/.ssh
+cat << EOF > /root/.ssh/config
+Host testy
+  HostName 127.0.0.1
+  ProxyCommand ssh proxy@127.0.0.1 -W 127.0.0.1:22
+EOF
 
 # set up root to know this host
-mkdir -p /root/.ssh
-ssh-keyscan -t rsa 127.0.0.1 > /root/.ssh/known_hosts
+ssh-keyscan 127.0.0.1 > /root/.ssh/known_hosts
+#ssh-keyscan -t rsa testy > /root/.ssh/known_hosts
 
-for user in "${@}"; do
-	ssh-keygen -t rsa -N "" -f "/root/.ssh/${user}"
-	make_user "${user}" "/root/.ssh/${user}.pub"
-	ssh  -i "/root/.ssh/${user}" "${user}@127.0.0.1" 'echo user $USER'
-done
+# make root's key
+ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
+
+make_user proxy /root/.ssh/id_rsa.pub
+su proxy -c 'ssh-keygen -t rsa -N "" -f /home/proxy/.ssh/id_rsa'
+su proxy -c 'ssh-keyscan 127.0.0.1 > /home/proxy/.ssh/known_hosts'
+
+make_user git /root/.ssh/id_rsa.pub
+
+  #ProxyCommand ssh proxy@127.0.0.1
+  #ProxyCommand ssh proxy@127.0.0.1 ssh git@127.0.0.1
+
+ssh git@testy 'echo $USER'
+cat /root/.ssh/known_hosts
