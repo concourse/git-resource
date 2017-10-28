8eabbd3f53c0c174cb28215ce2ff96766dd806c9
Josh Ghiloni <jghiloni@pivotal.io>
2017-10-28 13:08:22 -0600

    Add support for SSH-based access to git tunnelling through an HTTPS Proxy.
    Add proxytunnel to the image and necessary configurations to the scripts. Tests added and examples updated.

--- README.md
+++ README.md
@@ -75,9 +75,16 @@ Tracks the commits in a [git](http://git-scm.com/) repository.
 * `gpg_keyserver`: *Optional.* GPG keyserver to download the public keys from.
   Defaults to `hkp:///keys.gnupg.net/`.
 
+* `https_tunnel`: *Optional.* Information about an HTTPS proxy that will be used to tunnel SSH-based git commands over.
+  Has the following sub-properties:
+    * `proxy_host`: *Required.* The host name or IP of the proxy server
+    * `proxy_port`: *Required.* The proxy server's listening port
+    * `proxy_user`: *Optional.* If the proxy requires authentication, use this username
+    * `proxy_password`: *Optional.* If the proxy requires authenticat, use this password
+
 ### Example
 
-Resource configuration for a private repo:
+Resource configuration for a private repo with an HTTPS proxy:
 
 ``` yaml
 resources:
@@ -96,6 +103,11 @@ resources:
     - name: core.bigFileThreshold
       value: 10m
     disable_ci_skip: true
+    https_tunnel:
+      proxy_host: proxy-server.mycorp.com
+      proxy_port: 3128
+      proxy_user: myuser
+      proxy_password: myverysecurepassword
 ```
 
 Fetching a repo with only 100 commits of history:
@@ -221,6 +233,17 @@ Run the tests with the following command:
 ```sh
 docker build -t git-resource .
 ```
+
+#### Note about the integration tests
+
+If you want to run the integration tests, a bit more work is required. You will require
+an actual git repo to which you can push and pull, configured for SSH access. To do this,
+add two files to `integration-tests/ssh` (note that names **are** important):
+* `test_key`: This is the private key used to authenticate against your repo.
+* `test_repo`: This file contains one line of the form `test_repo_url[#test_branch]`.
+  If the branch is not specified, it defaults to `master`. For example,
+  `git@github.com:concourse-git-tester/git-resource-integration-tests.git` or
+  `git@github.com:concourse-git-tester/git-resource-integration-tests.git#testing`
 
 ### Contributing
 
---
+++ integration-tests/integration.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+set -e
+
+basedir="$(dirname "$0")"
+. "$basedir/../test/helpers.sh"
+. "$basedir/helpers.sh"
+
+it_can_check_through_a_tunnel() {
+  output=$(check_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$@" 2>&1)
+  echo "$output"
+
+  ( echo "$output" | grep 'Via localhost:3128 ->' >/dev/null 2>&1 )
+  rc=$?
+
+  test "$rc" -eq "0"
+}
+
+it_can_get_through_a_tunnel() {
+  get_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$@"
+}
+
+it_can_put_through_a_tunnel() {
+  get_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$@"
+
+  pushd $TMPDIR
+    git clone destination staging
+    date >> staging/test-file
+
+    pushd staging
+      git add .
+      git commit -m "Integration Test $(date)"
+    popd
+  popd
+
+  put_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination $TMPDIR/staging "$@"
+}
+
+it_can_check_through_a_tunnel_with_auth() {
+  it_can_check_through_a_tunnel "$basedir/tunnel/auth"
+}
+
+it_can_get_through_a_tunnel_with_auth() {
+  it_can_get_through_a_tunnel "$basedir/tunnel/auth"
+}
+
+it_can_put_through_a_tunnel_with_auth() {
+  it_can_put_through_a_tunnel "$basedir/tunnel/auth"
+}
+
+it_cant_check_through_a_tunnel_without_auth() {
+  set +e
+  test_auth_failure "$(check_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$@" 2>&1)"
+}
+
+it_cant_get_through_a_tunnel_without_auth() {
+  set +e
+  test_auth_failure "$(get_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$@" 2>&1)"
+}
+
+it_cant_put_through_a_tunnel_without_auth() {
+  get_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination "$basedir/tunnel/auth"
+  pushd $TMPDIR
+    git clone destination staging
+    date >> staging/test-file
+
+    pushd staging
+      git add .
+      git commit -m "Integration Test $(date)"
+    popd
+  popd
+
+  set +e
+  test_auth_failure "$(put_uri_with_private_key_and_tunnel_info "$(dirname "$0")/ssh/test_key" $TMPDIR/destination $TMPDIR/staging "$@" 2>&1)"
+}
+
+init_integration_tests $basedir
+run_with_unauthenticated_proxy "$basedir" it_can_check_through_a_tunnel
+run_with_unauthenticated_proxy "$basedir" it_can_get_through_a_tunnel
+run_with_unauthenticated_proxy "$basedir" it_can_put_through_a_tunnel
+run_with_unauthenticated_proxy "$basedir" it_can_check_through_a_tunnel_with_auth
+run_with_unauthenticated_proxy "$basedir" it_can_get_through_a_tunnel_with_auth
+run_with_unauthenticated_proxy "$basedir" it_can_put_through_a_tunnel_with_auth
+
+run_with_authenticated_proxy "$basedir" it_can_check_through_a_tunnel_with_auth
+run_with_authenticated_proxy "$basedir" it_can_get_through_a_tunnel_with_auth
+run_with_authenticated_proxy "$basedir" it_can_put_through_a_tunnel_with_auth
+run_with_authenticated_proxy "$basedir" it_cant_check_through_a_tunnel_without_auth
+run_with_authenticated_proxy "$basedir" it_cant_get_through_a_tunnel_without_auth
+run_with_authenticated_proxy "$basedir" it_cant_put_through_a_tunnel_without_auth
