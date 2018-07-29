c01a314bd7797a0f46a32c351b4c299e4cd4289d
Anthony Lucillo <alucillo@users.noreply.github.com>
2018-07-29 11:03:17 -0400

    Upgraded to latest git lfs version

--- scripts/install_git_lfs.sh
+++ scripts/install_git_lfs.sh
@@ -7,10 +7,10 @@ _main() {
   tmpdir="$(mktemp -d git_lfs_install.XXXXXX)"
 
   cd "$tmpdir"
-  curl -Lo git.tar.gz https://github.com/github/git-lfs/releases/download/v1.5.4/git-lfs-linux-amd64-1.5.4.tar.gz
+  curl -Lo git.tar.gz https://github.com/github/git-lfs/releases/download/v2.4.2/git-lfs-linux-amd64-2.4.2.tar.gz
   gunzip git.tar.gz
   tar xf git.tar
-  mv git-lfs-1.5.4/git-lfs /usr/bin
+  mv git-lfs-2.4.2/git-lfs /usr/bin
   cd ..
   rm -rf "$tmpdir"
   git lfs install --skip-smudge
