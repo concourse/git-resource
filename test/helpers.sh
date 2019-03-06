62f1204144b2e7c37a35bae1b322bf82491b8745
Alex Suraci <suraci.alex@gmail.com>
2019-03-06 00:03:25 +0000

    Revert "Merge pull request #237 from alphagov/submodule_recursive_source"
    This reverts commit bc45ab9562daf7e909f4e55917faf0226e37e48c, reversing
    changes made to b1db5e59cb6279bf36fa6054be1481b30a2db3e5.

--- test/helpers.sh
+++ test/helpers.sh
@@ -502,19 +502,6 @@ get_uri_with_submodules_and_parameter_remote() {
   }" | ${resource_dir}/in "$5" | tee /dev/stderr
 }
 
-get_uri_with_submodules_and_source_recursive() {
-  jq -n "{
-    source: {
-      uri: $(echo $1 | jq -R .),
-      submodule_recursive: $(echo $4 | jq -r .),
-    },
-    params: {
-      depth: $(echo $2 | jq -R .),
-      submodules: $(echo $3 | jq -R .),
-    }
-  }" | ${resource_dir}/in "$5" | tee /dev/stderr
-}
-
 get_uri_with_submodules_and_parameter_recursive() {
   jq -n "{
     source: {
@@ -523,23 +510,9 @@ get_uri_with_submodules_and_parameter_recursive() {
     params: {
       depth: $(echo $2 | jq -R .),
       submodules: $(echo $3 | jq -R .),
-      submodule_recursive: $(echo $4 | jq -r .),
+      submodule_recursive: $(echo $4 | jq -R .),
     }
   }" | ${resource_dir}/in "$5" | tee /dev/stderr
-}
-
-get_uri_with_submodules_and_source_and_parameter_recursive() {
-  jq -n "{
-    source: {
-      uri: $(echo $1 | jq -R .),
-      submodule_recursive: $(echo $4 | jq -r .),
-    },
-    params: {
-      depth: $(echo $2 | jq -R .),
-      submodules: $(echo $3 | jq -R .),
-      submodule_recursive: $(echo $5 | jq -r .),
-    }
-  }" | ${resource_dir}/in "$6" | tee /dev/stderr
 }
 
 get_uri_at_ref() {
