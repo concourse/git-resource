692642f560c30ca1d8466e1c1af1313ba6413871
Henry Chan <henry.chan@opendoor.com>
2023-10-11 16:29:13 -0700

    modified assets

--- assets/common.sh
+++ assets/common.sh
@@ -226,6 +226,11 @@ load_git_crypt_key() {
 }
 
 logInfo() {
+  GREEN='\033[0;32m'
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+
   message=$@
-  echo "<B>opendoor/git-resource INFO:</B> $message"
+  printf "${GREEN}opendoor/git-resource INFO:${NC} $message\n"
 }
+ 
