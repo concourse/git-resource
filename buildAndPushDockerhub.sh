28c7f8b4557f8884399ff7cfa979695429941d53
Henry Chan <henry.chan@opendoor.com>
2023-10-11 11:05:40 -0700

    added buildAndPushDockerhub.sh

---
+++ buildAndPushDockerhub.sh
@@ -0,0 +1,2 @@
+# docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
+docker buildx build --platform linux/amd64 -t opendoor/git-resource --build-arg base_image=paketobuildpacks/run-jammy-base:latest .
