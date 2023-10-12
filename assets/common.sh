118c7ba8ae14bff2edfd59fcc75d80afacb1ee72
Henry Chan <henry.chan@opendoor.com>
2023-10-11 20:26:51 -0700

    uses       url=$(echo $url | sed "s#https://.*\@#https://#")

--- assets/common.sh
+++ assets/common.sh
@@ -167,6 +167,7 @@ add_git_metadata_url() {
     esac
 
     if [ -n "$url" ]; then
+      url=$(echo $url | sed "s#https://.*\@#https://#")
       jq ". + [
         {name: \"url\", value: \"${url}\"}
       ]"
