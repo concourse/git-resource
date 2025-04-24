44c30051273358458376190f09bf87c6ffec9e73
C85297 <95289555+C85297@users.noreply.github.com>
2025-04-24 11:03:20 +0100

    Fix tags filtering resulting in paths filter being ignored
    Signed-off-by: C85297 <95289555+C85297@users.noreply.github.com>

--- test/check.sh
+++ test/check.sh
@@ -4,6 +4,16 @@ set -e
 
 source $(dirname $0)/helpers.sh
 
+check_jq_functionality() {
+  # Ensure JQ correctly treats empty input as invalid
+  set +e
+  jq -e 'type == "string"' </dev/null
+  if [ $? -eq 0 ]; then
+    echo "WARNING - Outdated JQ - please update! Some tests may incorrectly pass"
+  fi
+  set -e
+}
+
 it_can_check_from_head() {
   local repo=$(init_repo)
   local ref=$(make_commit $repo)
@@ -644,6 +654,97 @@ it_can_check_with_tag_filter() {
   "
 }
 
+it_can_check_with_tag_and_path_match_filter() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to_file $repo file-a)
+  local ref2=$(make_annotated_tag $repo "1.0-staging" "tag 1" true)
+  local ref3=$(make_commit_to_file $repo file-b)
+  local ref4=$(make_annotated_tag $repo "1.0-production" "tag 2" true)
+  local ref5=$(make_annotated_tag $repo "2.0-staging" "tag 3" true)
+  local ref6=$(make_commit_to_file $repo file-c)
+  local ref7=$(make_annotated_tag $repo "2.0-staging" "tag 5" true)
+  local ref8=$(make_commit_to_file $repo file-b)
+  local ref9=$(make_annotated_tag $repo "2.0-production" "tag 4" true)
+  local ref10=$(make_commit_to_file $repo file-c)
+
+  check_uri_with_tag_and_path_filter $repo "2*" "match_tagged" file-c | jq -e "
+    . == [{ref: \"2.0-staging\", commit: \"$ref6\"}]
+  "
+
+  # No matching files. ref3's tag was replaced by the tag on ref6 - which does not path match
+  check_uri_with_tag_and_path_filter $repo "*-staging" "match_tagged" file-b | jq -e "
+    . == []
+  "
+
+  # Although multiple tagged commits modify the files, ref8 is the latest (and version_depth is default 1)
+  check_uri_with_tag_and_path_filter $repo "*" "match_tagged" file-b file-c | jq -e "
+    . == [
+      {ref: \"2.0-production\", commit: \"$ref8\"}
+    ]
+  "
+
+  # file-f was never created
+  check_uri_with_tag_and_path_filter $repo "*" "match_tagged" file-f | jq -e "
+    . == []
+  "
+
+  # no tags matching 4.0-*
+  check_uri_with_tag_and_path_filter $repo "4.0-*" "match_tagged" file-c | jq -e "
+    . == []
+  "
+}
+
+it_can_check_with_tag_and_path_match_ancestors_filter() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to_file $repo file-a)
+  local ref2=$(make_annotated_tag $repo "1.0-staging" "tag 1" true)
+  local ref3=$(make_commit_to_file $repo file-b)
+  local ref4=$(make_annotated_tag $repo "1.0-production" "tag 2" true)
+  local ref5=$(make_annotated_tag $repo "2.0-staging" "tag 3" true)
+  local ref6=$(make_commit_to_file $repo file-c)
+  local ref7=$(make_annotated_tag $repo "2.0-staging" "tag 5" true)
+  local ref8=$(make_commit_to_file $repo file-b)
+  local ref9=$(make_annotated_tag $repo "2.0-production" "tag 4" true)
+  local ref10=$(make_commit_to_file $repo file-c)
+
+  # ref10 is the most recent to modify file-c, but the latest 2* tag is before it. Therefore ref6 matches.
+  check_uri_with_tag_and_path_filter $repo "2*" "match_tag_ancestors" file-c | jq -e "
+    . == [{ref: \"$ref6\"}]
+  "
+
+  # ref3 is the most recent commit to modify file-b, and following commits have -staging tags
+  check_uri_with_tag_and_path_filter $repo "*-staging" "match_tag_ancestors" file-b | jq -e "
+    . == [{ref: \"$ref3\"}]
+  "
+
+  # although no 2.* tagged commits modified file-a, they follow on from commits that did
+  check_uri_with_tag_and_path_filter $repo "2.*" "match_tag_ancestors" file-a | jq -e "
+    . == [{ref: \"$ref1\"}]
+  "
+
+  # no commits creating file-c are followed by 1.* tags
+  check_uri_with_tag_and_path_filter $repo "1.*" "match_tag_ancestors" file-c | jq -e "
+    . == []
+  "
+
+  # Although multiple tagged commits modify the files, ref8 is the latest (and version_depth is default 1)
+  check_uri_with_tag_and_path_filter $repo "*" "match_tag_ancestors" file-b file-c | jq -e "
+    . == [
+      {ref: \"$ref8\"}
+    ]
+  "
+
+  # file-f was never created
+  check_uri_with_tag_and_path_filter $repo "*" "match_tag_ancestors" file-f | jq -e "
+    . == []
+  "
+
+  # no tags matching 4.0-*
+  check_uri_with_tag_and_path_filter $repo "4.0-*" "match_tag_ancestors" file-c | jq -e "
+    . == []
+  "
+}
+
 it_can_check_with_tag_regex() {
   local repo=$(init_repo)
   local ref1=$(make_commit $repo)
@@ -679,7 +780,7 @@ it_can_check_with_tag_filter_with_cursor() {
   local ref13=$(make_commit $repo)
 
   x=$(check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging")
-  check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" | jq -e "
+  check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" 2 | jq -e "
     . == [{ref: \"2.0-staging\", commit: \"$ref5\"}, {ref: \"3.0-staging\", commit: \"$ref9\"}]
   "
 }
@@ -701,7 +802,7 @@ it_can_check_with_tag_regex_with_cursor() {
   local ref13=$(make_commit $repo)
 
   x=$(check_uri_with_tag_regex_from $repo ".*-staging" "2.0-staging")
-  check_uri_with_tag_regex_from $repo ".*-staging" "2.0-staging" | jq -e "
+  check_uri_with_tag_regex_from $repo ".*-staging" "2.0-staging" 2 | jq -e "
     . == [{ref: \"2.0-staging\", commit: \"$ref5\"}, {ref: \"3.0-staging\", commit: \"$ref9\"}]
   "
 }
@@ -765,7 +866,7 @@ it_can_check_with_tag_filter_over_all_branches_with_cursor() {
   local ref13=$(make_annotated_tag $repo "3.0-production" "tag 6")
   local ref14=$(make_commit_to_branch $repo branch-a)
 
-  check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" | jq -e "
+  check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" 2 | jq -e "
     . == [{ref: \"2.0-staging\", commit: \"$ref6\"}, {ref: \"3.0-staging\", commit: \"$ref10\"}]
   "
 }
@@ -787,7 +888,7 @@ it_can_check_with_tag_regex_over_all_branches_with_cursor() {
   local ref13=$(make_annotated_tag $repo "3.0-production" "tag 6")
   local ref14=$(make_commit_to_branch $repo branch-a)
 
-  check_uri_with_tag_regex_from $repo ".*-staging" "2.0-staging" | jq -e "
+  check_uri_with_tag_regex_from $repo ".*-staging" "2.0-staging" 2 | jq -e "
     . == [{ref: \"2.0-staging\", commit: \"$ref6\"}, {ref: \"3.0-staging\", commit: \"$ref10\"}]
   "
 }
@@ -1014,6 +1115,7 @@ it_checks_uri_with_tag_filter_and_version_depth() {
     ]"
 }
 
+run check_jq_functionality
 run it_can_check_from_head
 run it_can_check_from_a_ref
 run it_can_check_from_a_first_commit_in_repo
@@ -1054,6 +1156,8 @@ run it_can_check_with_tag_filter_with_bogus_ref
 run it_can_check_with_tag_regex_with_bogus_ref
 run it_can_check_with_tag_filter_with_replaced_tags
 run it_can_check_with_tag_regex_with_replaced_tags
+run it_can_check_with_tag_and_path_match_filter
+run it_can_check_with_tag_and_path_match_ancestors_filter
 run it_can_check_from_head_only_fetching_single_branch
 run it_can_check_and_set_git_config
 run it_can_check_from_a_ref_and_only_show_merge_commit
